bf7f160bb3d54d081a24dcbebdfe3c41786e1f9b
Ashok9642 <93345671+Ashok9642@users.noreply.github.com>
2026-03-19 21:53:11 +0530

    login functionality commit free files to dashboard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-
+import { useState } from 'react';
 import './App.css';
 import Hospital from './Hospital.js/Hospital';
 
-
-
 function App() {
+  const [value, setvalue1] = useState(0);
   return (
     <div className="App">
- <Hospital/>
+      <Hospital />
     </div>
   );
 }
